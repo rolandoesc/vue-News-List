7e82b2935563fc55f9d0713e8c8281dbd163204a
Rolando Escalona <rolando.escalona@gmail.com>
2018-01-20 00:13:00 -0600

    News list Vue App

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+   <SourceSelection v-on:sourceChanged="sourceChanged" />
+   <Newslist v-bind:source="source" />
+  </div>
+</template>
+
+<script>
+import SourceSelection from './components/SourceSelection'
+import Newslist from './components/Newslist'
+
+export default {
+  name: 'App',
+  components: {
+    SourceSelection,
+    Newslist
+  },
+  data() {
+    return {
+      source: ""
+    }
+  },
+  methods:{
+    sourceChanged: function (source) {
+      this.source = source;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  padding-top: 20px;
+}
+</style>
